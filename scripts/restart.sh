273d11c22afcb39f318a5f3fbfc0b195dfa467ea
石文文 <shiwenwendevelopment@gmail.com>
2023-04-21 10:12:46 +0000

    更新restart脚本

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,6 +1,8 @@
-if [ -f "shutdown.sh" ] && [ -f "start.sh" ]; then
-  sh shutdown.sh
-  sh start.sh
-else
-  echo "缺少启动脚本！！！"
-fi
+# 获取当前脚本所在目录的路径
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# 执行shutdown.sh脚本
+sh ${SCRIPT_DIR}/shutdown.sh
+echo "服务已关闭"
+# 执行start.sh脚本
+echo "服务开始启动"
+sh ${SCRIPT_DIR}/start.sh
