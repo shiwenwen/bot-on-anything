6b065a77860e555cc7f198c78911ed46a936a578
石文文 <shiwenwendevelop@163.com>
2023-04-22 10:10:47 +0800

    优化Bing开启jailbreak模式下的对话统计

--- model/bing/new_bing_model.py
+++ model/bing/new_bing_model.py
@@ -174,7 +174,7 @@ class BingModel(Model):
             if throttling["numUserMessagesInConversation"] == throttling["maxNumUserMessagesInConversation"]:
                 user_session.get(context['from_user_id'], None).reset()
                 throttling_str = "(对话轮次已达上限，本次聊天已结束，将开启新的对话)"
-            else:
+            elif not self.jailbreak: # 因为当开启 jailbreak 轮次始终为1 不需要统计
                 throttling_str = f"对话轮次: {throttling['numUserMessagesInConversation']}/{throttling['maxNumUserMessagesInConversation']}\n"
 
             response = f"{reply_text}\n{reference}\n{suggestion}\n***\n{throttling_str}"
