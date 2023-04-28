59993b02b1d378978e305841abdda8c89365603b
石文文 <shiwenwendevelopment@gmail.com>
2023-04-28 03:26:41 +0000

    Fix bug

--- scripts/startserver.sh
+++ scripts/startserver.sh
@@ -15,5 +15,5 @@ echo $BASE_DIR
 source ${BASE_DIR}/venv/bin/activate
 
 #nohup python3 "${BASE_DIR}/main.py" --debug=False >> ${BASE_DIR}/logs/log_info.log 2>&1 &
-nohup python3 "${BASE_DIR}/main.py" --host=80 --debug=False >> /dev/null 2>&1 &
+nohup python3 "${BASE_DIR}/main.py" --port=80 --debug=False >> /dev/null 2>&1 &
 echo "bot-on-anaything http server is starting"
