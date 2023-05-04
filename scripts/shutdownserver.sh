658eae4aeeddf0167735dce05cd8c97f2263273f
石文文 <shiwenwendevelopment@gmail.com>
2023-05-04 12:25:51 +0000

    修改端口

--- scripts/shutdownserver.sh
+++ scripts/shutdownserver.sh
@@ -2,7 +2,7 @@
 
 #关闭服务
 #查找端口为5000的进程，如果存在则杀掉，否则提示服务未启动，无需关闭，退出脚本，使用lsof -i:5000命令查看端口占用情况
- pid=`lsof -i:80 | grep LISTEN | awk '{print $2}'`
+ pid=`lsof -i:81 | grep LISTEN | awk '{print $2}'`
  if [ -z "$pid" ] ; then
          echo "No bot-on-anaything server running."
          exit 0;
